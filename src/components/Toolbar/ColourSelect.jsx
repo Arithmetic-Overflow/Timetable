f0c24540cab2ad6d2668d4a9be1df19062b0b499
Arithmetic-Overflow <aryageza@gmail.com>
2021-06-24 12:11:49 +0800

    Removed redundant state, fixed a bug where updates where lagging behind

--- src/components/Toolbar/ColourSelect.jsx
+++ src/components/Toolbar/ColourSelect.jsx
@@ -11,15 +11,11 @@ const ColourSelect = ({ className, isSelected, unitList, colourList }) => {
     [isSelected]
   );
 
-  const [units, setUnits] = useState(unitList);
-  const [unitColours, setUnitColours] = useState(colourList);
-
-
   const getSelections = () => {
     return (
-      units.map(
+      unitList.map(
         (unit, colourIndex) =>
-          [unit, unitColours[colourIndex]]
+          [unit, colourList[colourIndex]]
       )
     );
   }
@@ -28,40 +24,38 @@ const ColourSelect = ({ className, isSelected, unitList, colourList }) => {
 
   useEffect(
     () => {
-    	setUnits(unitList);
-    	setUnitColours(colourList);
     	setSelections(getSelections());
     },
     [unitList, colourList]
   );
 
-    return (
-				<DropdownButton
-					title='Select Colour'
-					variant='secondary'
-				>
-	        {
-	        	selections.map(
-	        		(selection) => {
-	        			const unitName 		= selection[0];
-	        			const colour 			= selection[1];
+  return (
+			<DropdownButton
+				title='Select Colour'
+				variant='secondary'
+			>
+        {
+        	selections.map(
+        		(selection) => {
+        			const unitName 		= selection[0];
+        			const colour 			= selection[1];
 
-	        			return (
-	        				<Dropdown.Item
-                    eventKey={ selection[0] } 
-                    key={ selection[0] }
-                    style={{
-                    	'backgroundColor': colour
-                    }}
-            			>
-              			{unitName}
-              		</Dropdown.Item>
-	        			);
-	        		}
-	        	)
-	        }
-				</DropdownButton>
-    );
+        			return (
+        				<Dropdown.Item
+                  eventKey={ selection[0] } 
+                  key={ selection[0] }
+                  style={{
+                  	'backgroundColor': colour
+                  }}
+          			>
+            			{unitName}
+            		</Dropdown.Item>
+        			);
+        		}
+        	)
+        }
+			</DropdownButton>
+  );
 };
 
 export default ColourSelect;
